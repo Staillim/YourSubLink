0e4a4c6e49495d19a5ba84d73cdc13155e5dc9f5
Staillim Gv <staillim12@gmail.com>
2025-07-29 19:44:26 +0000

    necesito que elimines la duplicacion y dejes solo el que funciona

--- app/[shortId]/page.tsx
+++
@@ -1,6 +0,0 @@
-
-// This file is intentionally left blank to resolve routing conflicts.
-// The primary logic has been consolidated into /src/app/link/[shortId]/page.tsx
-export default function DeprecatedShortLinkPage() {
-  return null;
-}
